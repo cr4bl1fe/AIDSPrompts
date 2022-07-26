878e1980f8ef6298f6e4d6b8955cad9c917a79b7
cr4bl1fe <crabl1fe@riseup.net>
2022-07-26 14:59:55 +0300

    fix website name in embed name

--- AIDungeonPromptsWeb/Constants/ItemsValueConstants.cs
+++ AIDungeonPromptsWeb/Constants/ItemsValueConstants.cs
@@ -3,5 +3,6 @@ namespace AIDungeonPrompts.Web.Constants
 	public static class ItemsValueConstants
 	{
 		public const string ColorPreferenceKey = "ColorPreference";
+		public const string WebsiteNameKey = "WebsiteName";
 	}
 }
